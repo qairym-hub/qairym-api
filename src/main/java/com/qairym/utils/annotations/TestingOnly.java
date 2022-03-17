87574fa3511867c3df510cc16c01191fe6cc9185
khanZhomart <jtalgatulu@yandex.kz>
2022-03-17 14:42:18 +0600

    Написал документацию к аннотации @TestingOnly

--- src/main/java/com/qairym/utils/annotations/TestingOnly.java
+++ src/main/java/com/qairym/utils/annotations/TestingOnly.java
@@ -6,7 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Methods marked with this annotation are designed only for developing purposes.
+ * A method annotated {@code @TestingOnly} is designed 
+ * only for developing purposes.
+ * 
+ * <p>It's strongly recommended to annotate such methods
+ * in order to define the production designed from 
+ * testing.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
